88af522761b278171624ea4858afb70519a00b26
jangwonyi <pinasun.naver.com>
2019-12-05 10:11:02 +0900

    pyautogui

---
+++ XI. 프로젝트/mecro1.py
@@ -0,0 +1,12 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    pag.moveTo(43,38, duration=2)
+    # pag.click()
+    # pag.click()
+    # pag.click(clicks=2)
+    # pag.doubleClick()
+    # pag.move(100, 200, duration=1)
+    # pag.rightClick()
+    # pag.click(button = "right")
+    pag.drag(0, 200, duration=2)
