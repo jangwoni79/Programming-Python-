e9a83a7b0433ca29bea4198c1244f85c4b9239d9
jangwonyi <pinasun.naver.com>
2019-12-09 08:43:37 +0900

    pyautogui - keyboard

--- XI. 프로젝트/mecro2.py
+++ XI. 프로젝트/mecro2.py
@@ -2,6 +2,7 @@ import pyautogui as pag
 import time     #sleep
 
 if __name__ == '__main__':
+    #time.sleep(2)
     pag.moveTo(200, 200, 2)
     pag.click()
     pag.typewrite("happy birㅎㅈthday to seungweon!", interval=0.2)
