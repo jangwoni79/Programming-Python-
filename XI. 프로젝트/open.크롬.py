604289d204a429540572a33309e0d540e16e504b
jangwonyi <pinasun.naver.com>
2019-12-09 09:54:56 +0900

    pyautogui - 크롬아이콘 여러개 인식하기

--- XI. 프로젝트/open.크롬.py
+++ XI. 프로젝트/open.크롬.py
@@ -2,12 +2,18 @@ import pyautogui as pag
 import time
 
 if __name__ == '__main__':
-    #크롬 이미지 인식
-    data = pag.locateOnScreen("크롬아이콘.PNG")
-    print(data)
-    #가운데 좌표 찾기
-    center = pag.center(data)
-    #마우스 이동
-    # pag.moveTo(center, duration=2)
-    #더블 클릭
-    pag.doubleClick(center, duration=2)
+    # #크롬 이미지 인식
+    # #center = pag.locationOnScreen("크롬아이콘.PNG")
+    # data = pag.locateOnScreen("크롬아이콘.PNG")
+    # print(data)
+    # #가운데 좌표 찾기
+    # center = pag.center(data)
+    # #마우스 이동
+    # # pag.moveTo(center, duration=2)
+    # #더블 클릭
+    # pag.doubleClick(center, duration=2)
+
+    data = pag.locateAllOnScreen("크롬아이콘.PNG")
+    for data in data:
+        center = pag.center(data)
+        pag.moveTo(center, duration = 0.2)
