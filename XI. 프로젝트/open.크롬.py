0d68dbe619e41ca7b3b308ad9880f635a69846f0
jangwonyi <pinasun.naver.com>
2019-12-09 09:49:04 +0900

    pyautogui - 크롬아이콘 인식하고, 가운데 구하고, 더블클릭하기

---
+++ XI. 프로젝트/open.크롬.py
@@ -0,0 +1,13 @@
+import pyautogui as pag
+import time
+
+if __name__ == '__main__':
+    #크롬 이미지 인식
+    data = pag.locateOnScreen("크롬아이콘.PNG")
+    print(data)
+    #가운데 좌표 찾기
+    center = pag.center(data)
+    #마우스 이동
+    # pag.moveTo(center, duration=2)
+    #더블 클릭
+    pag.doubleClick(center, duration=2)
